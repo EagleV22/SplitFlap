552826570058e71ba18df9646be0e31ef5125c3a
yveshunziker <yves.hunziker@students.fhnw.ch>
2024-03-20 12:01:31 +0100

    changed flip logic, letter behind visible

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 function flipLetter() {
     const topFlap = document.querySelector('.split-flap .top');
     const bottomFlap = document.querySelector('.split-flap .bottom');
-    const newChar = getNextChar(topFlap.textContent.trim());
+    const shadowFlap = document.querySelector('.split-flap .shadow'); // Zugriff auf die Schattenkarte
+    const newCharBottom = getNextCharBottom(topFlap.querySelector('.letter').textContent.trim());
+
+    /
+    shadowFlap.querySelector('.letter').textContent = getNextCharBottom(newCharBottom);
 
     topFlap.style.transform = 'rotateX(-180deg)';
+    topFlap.style.zIndex = 2;
 
     setTimeout(() => {
-        topFlap.querySelector('.letter').textContent = newChar;
-        bottomFlap.querySelector('.letter').textContent = newChar;
+        topFlap.querySelector('.letter').textContent = newCharBottom;
+        bottomFlap.querySelector('.letter').textContent = newCharBottom;
         topFlap.style.transform = 'rotateX(0deg)';
-    }, 700);
+        topFlap.style.zIndex = 1;
+    }, 200);
 }
 
-function getNextChar(currentChar) {
+function getNextCharBottom(currentChar) {
     const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const currentIndex = alphabet.indexOf(currentChar);
     return alphabet[(currentIndex + 1) % alphabet.length];
 }
+
+
 
 
 
