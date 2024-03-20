7602c7457d51614419df6bb257fb79b5c71b2831
marc <marc.foery@gmail.com>
2024-03-20 13:19:05 +0100

    comments

--- index.js
+++ index.js
@@ -5,30 +5,25 @@ function flipLetter() {
     const currentChar = topFlap.querySelector('.letter').textContent.trim();
     const newChar = getNextCharBottom(currentChar);
 
-    // Vorbereitung der Schattenkarte mit dem nächsten Buchstaben
     shadowFlap.querySelector('.letter').textContent = getNextCharBottom(newChar);
 
     topFlap.style.transform = 'rotateX(-180deg)';
     topFlap.style.zIndex = 2;
 
     setTimeout(() => {
-        // Die aktuelle obere Karte wird mit dem neuen Buchstaben aktualisiert
         topFlap.querySelector('.letter').textContent = newChar;
-        // Die untere Karte wird mit demselben neuen Buchstaben aktualisiert, um Konsistenz zu wahren
         bottomFlap.querySelector('.letter').textContent = newChar;
 
-        // Setzen Sie die Transformation der oberen Karte zurück, ohne sie visuell zurückzuklappen
-        topFlap.classList.add('notransition'); // Deaktivieren Sie vorübergehend Transitionen
+        topFlap.classList.add('notransition');
         topFlap.style.transform = 'rotateX(0deg)';
-        topFlap.offsetHeight; // Trigger reflow
-        topFlap.classList.remove('notransition'); // Aktivieren Sie Transitionen wieder
+        topFlap.offsetHeight;
+        topFlap.classList.remove('notransition');
 
         topFlap.style.zIndex = 1;
 
-        // Die Schattenkarte wird für den nächsten Buchstaben vorbereitet
         const nextChar = getNextCharBottom(newChar);
         shadowFlap.querySelector('.letter').textContent = nextChar;
-    }, 200);
+    }, 700);
 }
 
 
