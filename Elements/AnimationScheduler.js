350f6216c39e911932da2b3b1c9529eddd649dcc
Clara Desktop <clara.tran@students.fhnw.ch>
2024-04-24 10:24:09 +0200

    remove unused parameter

--- Elements/AnimationScheduler.js
+++ Elements/AnimationScheduler.js
@@ -3,30 +3,25 @@ class AnimationScheduler {
         this.tasks = [];
     }
 
-    // Add a task to the queue
-    addAnimation(task) {
+      addAnimation(task) {
         this.tasks.push(task);
     }
 
-    // Start all animations sequentially
     startAllAnimations(callback) {
         let index = 0;
 
-        // Function to execute tasks one after another
         const executeTask = () => {
             if (index < this.tasks.length) {
                 const task = this.tasks[index++];
-                // Each task must call executeTask when it's done
                 task(executeTask);
             } else {
-                // All tasks are completed, clear the tasks list
                 this.tasks = [];
                 if (typeof callback === 'function') {
-                    callback(); // Call the callback if provided
+                    callback();
                 }
             }
         };
 
-        executeTask(); // Start executing from the first task
+        executeTask();
     }
 }
